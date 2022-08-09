137b1180042cbfca3bffda68b37a289a8fa45a2a
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-09 13:11:39 +0300

    adding navigation bar,images

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Learning code is not for the weak!";
+h2.textContent = "Love Yourself Enough To Live a Healthy Life!";
 document.querySelector("body").appendChild(h2);
